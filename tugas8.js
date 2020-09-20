0c11ef1ed196880f17c45cc59a8a4ba5b47eca54
budiman993 <71246269+budiman993@users.noreply.github.com>
2020-09-20 15:48:05 +0700

    Add files via upload

---
+++ tugas8.js
@@ -0,0 +1,11 @@
+var motor = {
+	type :  "Bebek",
+	harga : "Rp. 15.000.000,-",
+	warna : "Hitam",
+}
+
+console.log("Data Motor Before :", motor)
+
+motor.harga = "Rp. 14.000.000,-";
+
+console.log("Data Motor After :", motor)
